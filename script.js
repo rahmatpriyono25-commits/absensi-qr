f8f91bcad6e8ff539ce6853f81c1bb2d63958d84
rahmatpriyono25-commits <rahmat.priyono25@admin.sd.belajar.id>
2026-06-26 12:42:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document
+.getElementById("btnStart")
+.addEventListener("click",function(){
 
+document.getElementById("status").innerHTML="Kamera Aktif";
+
+alert("Tahap berikutnya kita hidupkan kamera.");
+
+});
