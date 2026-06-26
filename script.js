102745e7bd39c79bd7aefe44442416779d0a05a6
rahmatpriyono25-commits <rahmat.priyono25@admin.sd.belajar.id>
2026-06-26 12:51:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.getElementById("btnStart").addEventListener("click", function () {
-    alert("Tombol berhasil diklik");
+document.getElementById("btnStart").addEventListener("click", async function () {
+
+    alert("Langkah 1");
+
+    try {
+
+        const cameras = await Html5Qrcode.getCameras();
+
+        alert("Jumlah kamera: " + cameras.length);
+
+        console.log(cameras);
+
+    } catch (err) {
+
+        alert("ERROR: " + err);
+
+        console.error(err);
+
+    }
+
 });
