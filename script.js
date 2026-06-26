093f78df11f0e9b142f2558115ae40642371f370
rahmatpriyono25-commits <rahmat.priyono25@admin.sd.belajar.id>
2026-06-26 12:44:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-document
-.getElementById("btnStart")
-.addEventListener("click",function(){
+let html5QrCode = null;
 
-document.getElementById("status").innerHTML="Kamera Aktif";
+document.getElementById("btnStart").addEventListener("click", async function () {
 
-alert("Tahap berikutnya kita hidupkan kamera.");
+    const status = document.getElementById("status");
+
+    try {
+
+        const cameras = await Html5Qrcode.getCameras();
+
+        if (cameras.length === 0) {
+            status.innerHTML = "Tidak ada kamera ditemukan";
+            return;
+        }
+
+        status.innerHTML = "Kamera Aktif";
+
+        html5QrCode = new Html5Qrcode("reader");
+
+        await html5QrCode.start(
+            cameras[0].id,
+            {
+                fps: 10,
+                qrbox: {
+                    width: 250,
+                    height: 250
+                }
+            },
+            function(decodedText) {
+
+                alert("QR Terbaca : " + decodedText);
+
+            }
+        );
+
+    } catch(err){
+
+        console.error(err);
+
+        status.innerHTML = err;
+
+    }
 
 });
