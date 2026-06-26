b55d2b67c9141bb1cfd8f7b68304c0d715a6597a
rahmatpriyono25-commits <rahmat.priyono25@admin.sd.belajar.id>
2026-06-26 13:52:28 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,30 @@
+async function kirimAbsen(nis) {
 
+    try {
+
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "text/plain;charset=utf-8"
+            },
+            body: JSON.stringify({
+                nis: nis
+            })
+        });
+
+        const hasil = await response.json();
+
+        return hasil;
+
+    } catch (err) {
+
+        console.error(err);
+
+        return {
+            success: false,
+            pesan: err.toString()
+        };
+
+    }
+
+}
