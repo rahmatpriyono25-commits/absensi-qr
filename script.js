8c3cfcc3bcf5fc525b172a92040056294032b81c
Bang Culers <rahmat.priyono25@admin.sd.belajar.id>
2026-06-27 15:53:18 +0800

    Perbaiki urutan script pada index.html

--- script.js
+++ script.js
@@ -4,71 +4,73 @@ let scannerRunning = false;
 const btnStart = document.getElementById("btnStart");
 const status = document.getElementById("status");
 const pesan = document.getElementById("pesan");
-
 btnStart.addEventListener("click", startScanner);
-
 async function startScanner() {
-
     if (scannerRunning) return;
-
     try {
-
         const cameras = await Html5Qrcode.getCameras();
 
         if (!cameras.length) {
             status.textContent = "Tidak ada kamera";
             return;
         }
-
         if (!scanner) {
             scanner = new Html5Qrcode("reader");
         }
-
         scannerRunning = true;
-
         await scanner.start(
-
-            { facingMode: "environment" },
+            cameras[0].id,
 
             {
                 fps: 10,
                 qrbox: 250
             },
+
             onScanSuccess,
+
             () => {
                 // abaikan error pembacaan
             }
+
         );
+
         status.textContent = "Kamera Aktif";
     } catch (err) {
+
         console.error(err);
         status.textContent = err.message;
     }
+
 }
+
 async function onScanSuccess(decodedText) {
     if (!scannerRunning) return;
     scannerRunning = false;
     status.textContent = "Mengirim absensi...";
     try {
         await scanner.pause(true);
+        console.log("Scanner dipause");
         const hasil = await kirimAbsen(decodedText);
         if (hasil.success) {
             document.getElementById("nama").textContent = hasil.nama;
             document.getElementById("nis").textContent = hasil.nis;
             document.getElementById("kelas").textContent = hasil.kelas;
             pesan.textContent =
-                hasil.pesan + " (" + hasil.jam + ")";
+            hasil.pesan + "(" + hasil.jam + ")";
             status.textContent = "Absensi Berhasil";
         } else {
             pesan.textContent = hasil.pesan;
             status.textContent = "Absensi Gagal";
         }
         setTimeout(async () => {
+            console.log("Resume scanner...");
             document.getElementById("nama").textContent = "-";
             document.getElementById("nis").textContent = "-";
             document.getElementById("kelas").textContent = "-";
             document.getElementById("pesan").textContent = "Menunggu QR Code...";
             await scanner.resume();
+            // console.log("Resume selesai");
+            console.log("Scanner diresume");
             scannerRunning = true;
             status.textContent = "Kamera Aktif";
         }, 2000);
